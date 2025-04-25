9b1f9e81e073dad65e03e5f503e6510ff3d2fd1c
RamaSandhy <rshandy07@gmail.com>
2025-04-25 11:10:13 +0700

    menambahkan login page layout dengan welcome text dan icon

--- ucp1flutterr/lib/login_page.dart
+++ ucp1flutterr/lib/login_page.dart
@@ -7,3 +7,23 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final emailController = TextEditingController();
     final passwordController = TextEditingController();
+
+     return Scaffold(
+      backgroundColor: const Color(0xFFFDF5F2), // warna background mirip
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Spacer(),
+              const Icon(Icons.navigation, size: 64, color: Colors.black),
+              const SizedBox(height: 16),
+              const Text(
+                'SELAMAT DATANG KEMBALI',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+              const SizedBox(height: 32),

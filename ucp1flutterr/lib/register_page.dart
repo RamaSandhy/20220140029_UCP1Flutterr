9e986c57433291cac949191af65ebdaddf49c5ec
RamaSandhy <rshandy07@gmail.com>
2025-04-29 18:03:31 +0700

    Menambahkan routing untuk halaman login dan register

--- ucp1flutterr/lib/register_page.dart
+++ ucp1flutterr/lib/register_page.dart
@@ -8,3 +8,21 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        primaryColor: const Color(0xFFFF5722),
+        scaffoldBackgroundColor: const Color(0xFFFDF5F2),
+      ),
+      routes: {
+        '/': (context) => const LoginPage(),
+        '/register': (context) => const RegisterPage(),
+      },
+      initialRoute: '/',
+    );
+  }
+}

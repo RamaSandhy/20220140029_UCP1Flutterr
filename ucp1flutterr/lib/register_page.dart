412fa1f1e89b7f4d971938c6f5f54a0d7878e020
RamaSandhy <rshandy07@gmail.com>
2025-04-29 18:46:08 +0700

    Menambahkan container dengan border dan radius untuk input Email

--- ucp1flutterr/lib/register_page.dart
+++ ucp1flutterr/lib/register_page.dart
@@ -104,4 +104,11 @@ class _RegisterPageState extends State<RegisterPage> {
                               fontWeight: FontWeight.w500,
                             ),
                           ),
+                          const SizedBox(height: 8),
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8),
+                              color: Colors.white,
+                              border: Border.all(color: Colors.grey.shade300),
+                            ),
 
